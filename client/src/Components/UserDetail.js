d155d10140a1a37157ed45cd0c24437351e23992
dcho0723 <dcho0723@yahoo.com>
2021-10-06 23:15:32 -0400

    added form to user Challenge

--- client/src/Components/UserDetail.js
+++ client/src/Components/UserDetail.js
@@ -25,90 +25,111 @@ function UserDetail({ allUsers, user, getTheData }) {
         sum += otherUser.players[i].rating;
       }
       return Math.round(sum / otherUser.players.length);
+    });
+
+  function handleClickChallenge() {
+    let ratingDifference = user.get_users_average_rating - userAverage[0];
+    if (ratingDifference <= 10 && ratingDifference >= -10) {
+      ratingDifference *= 4;
+    } else {
+      ratingDifference = user.get_users_average_rating - userAverage[0];
     }
+
+    let randomNumberForDifference = Math.floor(
+      Math.random() * ratingDifference
     );
+    let randomPlayer =
+      user.players[Math.floor(Math.random() * user.players.length)].name;
 
-    function handleClickChallenge() {
-        let ratingDifference = user.get_users_average_rating - userAverage[0]
-        if  (ratingDifference <= 10 && ratingDifference >= -10) {
-            ratingDifference *= 4
-        } else {
-            ratingDifference = user.get_users_average_rating - userAverage[0]
-        }
+    let winningArr = [
+      `What a game! ${randomPlayer} played at his best`,
+      `${randomPlayer} played well today!`,
+      `${randomPlayer} let his team to victory`,
+      `${randomPlayer} played a big role in todays win! `,
+    ];
 
-        let randomNumberForDifference = Math.floor(Math.random() * ratingDifference)
-        let randomPlayer = user.players[Math.floor(Math.random() * user.players.length)].name
-        
-        let winningArr = [`What a game! ${randomPlayer} played at his best`, `${randomPlayer} played well today!`, `${randomPlayer} let his team to victory`, `${randomPlayer} played a big role in todays win! `]
+    let losingArr = [
+      `Tough loss. ${randomPlayer} wasn't at their best`,
+      `${randomPlayer} mistake led to a costly lost.`,
+      `Rough night for ${randomPlayer}.`,
+      `${randomPlayer} couldn't seem to get a control of the game.`,
+    ];
 
-        let losingArr = [`Tough loss. ${randomPlayer} wasn't at their best`, `${randomPlayer} mistake led to a costly lost.`, `Rough night for ${randomPlayer}.`, `${randomPlayer} couldn't seem to get a control of the game.`]
+    let winningStr = winningArr[Math.floor(Math.random() * winningArr.length)];
 
-        let winningStr = winningArr[Math.floor(Math.random() * winningArr.length)]
+    let losingStr = losingArr[Math.floor(Math.random() * losingArr.length)];
 
-        let losingStr = losingArr[Math.floor(Math.random() * losingArr.length)]
+    let firstRandomScore = Math.floor(Math.random() * 4);
+    let secondRandomScore =
+      firstRandomScore + Math.floor(Math.random() * 3) + 1;
 
-        let firstRandomScore = Math.floor(Math.random() * 4)
-        let secondRandomScore = firstRandomScore + Math.floor(Math.random() * 3) + 1 
-
-        if (randomNumberForDifference<=10 && randomNumberForDifference >= -10) {
-        // console.log("i won")
-        window.alert(`${winningStr} ${user.name} won ${secondRandomScore} - ${firstRandomScore}!`)
-        winningPatch()
-        } else {
-            window.alert(`${losingStr} ${user.name} lost ${firstRandomScore} - ${secondRandomScore}!`)
-            // console.log("you lost")
-            losingPatch()
-        }
+    if (randomNumberForDifference <= 10 && randomNumberForDifference >= -10) {
+      // console.log("i won")
+      window.alert(
+        `${winningStr} ${user.name} won ${secondRandomScore} - ${firstRandomScore}!`
+      );
+      winningPatch();
+    } else {
+      window.alert(
+        `${losingStr} ${user.name} lost ${firstRandomScore} - ${secondRandomScore}!`
+      );
+      // console.log("you lost")
+      losingPatch();
     }
+  }
 
+  function winningPatch() {
+    fetch(`/players/${playerId}/update`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        pace: (pace += Math.floor(Math.random() * 4)),
+        shot: (shot += Math.floor(Math.random() * 4)),
+        pass: (pass += Math.floor(Math.random() * 4)),
+        dribble: (dribble += Math.floor(Math.random() * 4)),
+        defence: (defence += Math.floor(Math.random() * 4)),
+        physical: (physical += Math.floor(Math.random() * 4)),
+        rating: (rating += Math.floor(Math.random() * 2)),
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        getTheData();
+      });
+  }
+  function losingPatch() {
+    fetch(`/players/${playerId}/update`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        pace: (pace -= Math.floor(Math.random() * 4)),
+        shot: (shot -= Math.floor(Math.random() * 4)),
+        pass: (pass -= Math.floor(Math.random() * 4)),
+        dribble: (dribble -= Math.floor(Math.random() * 4)),
+        defence: (defence -= Math.floor(Math.random() * 4)),
+        physical: (physical -= Math.floor(Math.random() * 4)),
+        rating: (rating -= Math.floor(Math.random() * 2)),
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        getTheData();
+      });
+  }
 
-
-    function winningPatch() {
-        fetch(`/players/${playerId}/update`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify({
-            pace: (pace += Math.floor(Math.random() * 4)),
-            shot: (shot += Math.floor(Math.random() * 4)),
-            pass: (pass += Math.floor(Math.random() * 4)),
-            dribble: (dribble += Math.floor(Math.random() * 4)),
-            defence: (defence += Math.floor(Math.random() * 4)),
-            physical: (physical += Math.floor(Math.random() * 4)),
-            rating: (rating += Math.floor(Math.random() * 2)),
-          }),
-        })
-          .then((r) => r.json())
-          .then((data) => {
-            console.log(data)
-            getTheData()
-          });
-      }
-      function losingPatch() {
-        fetch(`/players/${playerId}/update`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify({
-            pace: (pace -= Math.floor(Math.random() * 4)),
-            shot: (shot -= Math.floor(Math.random() * 4)),
-            pass: (pass -= Math.floor(Math.random() * 4)),
-            dribble: (dribble -= Math.floor(Math.random() * 4)),
-            defence: (defence -= Math.floor(Math.random() * 4)),
-            physical: (physical -= Math.floor(Math.random() * 4)),
-            rating: (rating -= Math.floor(Math.random() * 2)),
-          }),
-        })
-          .then((r) => r.json())
-          .then((data) => {
-            console.log(data)
-            getTheData()
-          });
-      }
+  function handleSubmit(e) {
+    e.preventDefault()
+    window.alert(`Message Sent`)
+    e.target.reset()
+  }
 
   return (
     <div>
@@ -121,57 +142,87 @@ function UserDetail({ allUsers, user, getTheData }) {
                 <h1>
                   {otherUser.name}'s Team Average Rating: {userAverage}
                 </h1>
-                <button onClick={handleClickChallenge} className="challengeBttn">Challenge?</button>
+                <button
+                  onClick={handleClickChallenge}
+                  className="challengeBttn"
+                >
+                  Challenge?
+                </button>
                 <br />
                 {otherUser.players.map((players) => {
                   return (
                     <>
-                    {players.position == "Goalie" ? (
-                      <div className="playerCard">
-                        <h1 className="playerName">{players.name}</h1>
-                        <h2 className="ratingName">Rating: {players.rating}</h2>
-                        <div style={{ textAlign: "center" }}>
-                          <img src={players.image} className="playerImg" />
-                          <h3 className="playerPosition">Position: {players.position}</h3>
+                      {players.position == "Goalie" ? (
+                        <div className="playerCard">
+                          <h1 className="playerName">{players.name}</h1>
+                          <h2 className="ratingName">
+                            Rating: {players.rating}
+                          </h2>
+                          <div style={{ textAlign: "center" }}>
+                            <img src={players.image} className="playerImg" />
+                            <h3 className="playerPosition">
+                              Position: {players.position}
+                            </h3>
+                          </div>
+                          <div className="goalieStat">
+                            <p>Diving: {players.pace}</p>
+                            <p>Handling: {players.shot}</p>
+                            <p>Kicking: {players.pass}</p>
+                          </div>
+                          <div>
+                            <p>Reflexes: {players.dribble}</p>
+                            <p>Speed: {players.defence}</p>
+                            <p>Positioning: {players.physical}</p>
+                          </div>
                         </div>
-                        <div className="goalieStat">
-                          <p>Diving: {players.pace}</p>
-                          <p>Handling: {players.shot}</p>
-                          <p>Kicking: {players.pass}</p>
+                      ) : (
+                        <div className="playerCard">
+                          <h1 className="playerName">{players.name}</h1>
+                          <h2 className="ratingName">
+                            Rating: {players.rating}
+                          </h2>
+                          <div style={{ textAlign: "center" }}>
+                            <img src={players.image} className="playerImg" />
+                            <h3 className="playerPosition">
+                              Position: {players.position}
+                            </h3>
+                          </div>
+                          <div className="playerStat">
+                            <p>Pace: {players.pace}</p>
+                            <p>Shot: {players.shot}</p>
+                            <p>Pass: {players.pass}</p>
+                          </div>
+                          <div>
+                            <p>Dribble: {players.dribble}</p>
+                            <p>Defence: {players.defence}</p>
+                            <p>Physical: {players.physical}</p>
+                          </div>
                         </div>
-                        <div>
-                          <p>Reflexes: {players.dribble}</p>
-                          <p>Speed: {players.defence}</p>
-                          <p>Positioning: {players.physical}</p>
-                        </div>
-                      </div>
-                    ) : (
-                      <div className="playerCard">
-                        <h1 className="playerName">{players.name}</h1>
-                        <h2 className="ratingName">Rating: {players.rating}</h2>
-                        <div style={{ textAlign: "center" }}>
-                          <img src={players.image} className="playerImg" />
-                          <h3 className="playerPosition">Position: {players.position}</h3>
-                        </div>
-                        <div className="playerStat">
-                          <p>Pace: {players.pace}</p>
-                          <p>Shot: {players.shot}</p>
-                          <p>Pass: {players.pass}</p>
-                        </div>
-                        <div>
-                          <p>Dribble: {players.dribble}</p>
-                          <p>Defence: {players.defence}</p>
-                          <p>Physical: {players.physical}</p>
-                        </div>
-                      </div>
-                    )}
+                      )}
                     </>
                   );
                 })}
               </div>
             );
           } else {
-            return <h1>{otherUser.name} hasn't created a team yet</h1>;
+            return (
+              <div>
+                <h1 className="noTeamH1">
+                  {otherUser.name} hasn't created a team yet
+                </h1>
+                <form className="userForm" onSubmit={handleSubmit}>
+                  <label>
+                    <textarea
+                      className="message"
+                      type="text"
+                      placeholder="Send A Message"
+                    />
+                    <br />
+                    <input type="submit" value="Submit" />
+                  </label>
+                </form>
+              </div>
+            );
           }
         })}
     </div>
